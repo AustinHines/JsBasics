9b0e3f4b8f82ca724d353fa45e4d02deee9a1414
Austin Hines <Austinhines0605@gmail.com>
2021-02-22 12:35:17 -0600

    Finished all but challenge

---
+++ jsBasicExercise.js
@@ -0,0 +1,27 @@
+15;
+
+//numbers, strings, booleans, null, undenied
+
+// +, -, *, /, %
+
+console.log(1%5);
+console.log(5%2);
+
+console.log('I\m marry poppins ya\ll');
+
+console.log("wanda" + "Vison");
+
+console.log("Kenneth".length + "Hines".length);
+
+console.log("Avengers: Endgame"[16]);
+
+console.log('\\');
+
+console.log(5**4-9);
+
+console.log("i" + "am" + "ironman"); //
+
+console.log('CAP ' + 'VISION ' + 'HAWKEYE ' + 'NICK FURY' + 'GROOT ' + 'DOCTOR STRANGE ' + 'THOR ' + 'STAR-LORD ');
+
+
+
